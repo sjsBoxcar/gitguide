8e018053dacc00602b0f1eb9f788ea119e323a53
NJHouse_SDMac <sjs_boxcar@NJHouse-SDMacui-MacBookPro.local>
2020-07-10 15:05:04 +0900

    컨플예제작성파일생성

---
+++ common.php
@@ -0,0 +1,18 @@
+<?
+
+function forLogin() 
+{
+    $fx = 1;
+    $fy = 3;
+    $fz = $fx + $fy;
+    return $fz;
+}
+
+function forBoard()
+{
+    $fa = 1;
+    $fb = 4;
+    $fc = $fa + $fb;
+    return $fc;
+}
+?>
